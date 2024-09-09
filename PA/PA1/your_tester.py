7b353c4f2e56f5dffb85cf8a63977a64ecfeba2d
bstrdlpn <110487349+bstrdlpn@users.noreply.github.com>
2024-09-08 20:49:08 -0700

    Create your_tester.py

---
+++ PA/PA1/your_tester.py
@@ -0,0 +1,19 @@
+from pa1 import equiv_to
+from pa1 import b_rep
+from pa1 import binary_add
+from pa1 import binary_mul
+
+
+def test():
+    print("Testing Problem 1...")
+    # Write your test cases for Problem 1 here
+
+    print("Testing Problem 2...")
+    # Write your test cases for Problem 2 here
+
+    print("Testing Problem 3...")
+    # Write your test cases for Problem 3 here
+
+    print("Testing Problem 4...")
+    # Write your test cases for Problem 4 here
+    return 
