8eea97c3fc63f0b6f77be02f8fae279cfe08626f
bstrdlpn <110487349+bstrdlpn@users.noreply.github.com>
2024-09-20 18:24:19 -0700

    Create your_tester.py

---
+++ PA/PA2/your_tester.py
@@ -0,0 +1,20 @@
+from pa2 import gcd, primes, bezout_coeffs, mod_inv, solve_mod_equiv
+
+
+def test():
+    print("Testing Problem 1...")
+    # Write your test cases for Problem 1 here
+
+    print("Testing Problem 2...")
+    # Write your test cases for Problem 2 here
+
+    print("Testing Problem 3...")
+    # Write your test cases for Problem 3 here
+
+    print("Testing Problem 4...")
+    # Write your test cases for Problem 4 here
+
+    print("Testing Problem 5...")
+    # Write your test cases for Problem 5 here
+
+
