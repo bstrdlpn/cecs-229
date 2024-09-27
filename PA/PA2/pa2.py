a9384946d6ab838d1587f54cf84ed25ef18c65b4
bstrdlpn <nguyenphanlong@gmail.com>
2024-09-27 11:19:04 -0700

    bezout_coeffs: WIP, editing logic

--- PA/PA2/pa2.py
+++ PA/PA2/pa2.py
@@ -2,85 +2,84 @@
 
 
 def primes(a, b):
-  """
-      prints all primes in the range [a, b]
-      :param a: int type; a positive integer greater than 1
-      :param b: int type; a positive integer greater than or equal to a.
-      :return: set type; a set of all primes in the range [a, b]
-      :raises ValueError if a < 1 or b < a
-      """
-  if a < 1 or b < a:  # handling invalid range
-    raise ValueError("Invalid range given")
+    """
+    prints all primes in the range [a, b]
+    :param a: int type; a positive integer greater than 1
+    :param b: int type; a positive integer greater than or equal to a.
+    :return: set type; a set of all primes in the range [a, b]
+    :raises ValueError if a < 1 or b < a
+    """
+    if a < 1 or b < a:  # handling invalid range
+      raise ValueError("Invalid range given")
 
-  if a == 1:  # handling starting point a = 1
-    a = 2  # this ensures 1 is not listed as a prime
+    if a == 1:  # handling starting point a = 1
+      a = 2  # this ensures 1 is not listed as a prime
 
-  # FIXME: initialize `stop` which is the stopping criteria for
-  #        the loop in the Sieve of Eratosthenes
-  stop = int(pow(b, 0.5) + 1)
+    # FIXME: initialize `stop` which is the stopping criteria for
+    #        the loop in the Sieve of Eratosthenes
+    stop = int(pow(b, 0.5) + 1)
 
-  # FIXME: initialize a Python set called `P` that contains
-  #        all integers in the range [a, b]
-  P = set(x for x in range(a,(b+1)))
+    # FIXME: initialize a Python set called `P` that contains
+    #        all integers in the range [a, b]
+    P = set(x for x in range(a,(b+1)))
 
-  for x in range(2, stop):
-    
-    # FIXME: use Python list comprehension to create a set
-    #        of multiples of x in the range [2, b];
-    # HINT: the set of multiples of x can be expressed as
-    #       k * x, where k is an integer; hence the comprehension
-    #       should loop over values that satisfy k * x <= b
-    multiples_x ={x * n for n in range(a // x, (b // x) + 1) if x * n <= b and x * n != x}
+    for x in range(2, stop):
+      # FIXME: use Python list comprehension to create a set
+      #        of multiples of x in the range [2, b];
+      # HINT: the set of multiples of x can be expressed as
+      #       k * x, where k is an integer; hence the comprehension
+      #       should loop over values that satisfy k * x <= b
+        multiples_x ={x * n for n in range(a // x, (b // x) + 1) if x * n <= b and x * n != x}
 
-    P -= multiples_x  # removing the multiples of x from the set P
+        P -= multiples_x  # removing the multiples of x from the set P
 
-  return P
+    return P
 
 
 """ ----------------- PROBLEM 2 ----------------- """
 
 
 def bezout_coeffs(a, b):
-  """
-      computes the Bezout coefficients of two given positive integers
-      :param a: int type; positive integer
-      :param b: int type; positive integer
-      :returns: dict type; a dictionary with parameters a and b as keys,
-                and their corresponding Bezout coefficients as values.
-      :raises: ValueError if a < 0 or b < 0
-      """
-  if a < 0 or b < 0:
-    raise ValueError(
-      f"bezout_coeffs(a, b) does not support negative arguments.")
-  s0 = 1
-  t0 = 0
-  s1 = -1 * (b // a)
-  t1 = 1
+    """
+    computes the Bezout coefficients of two given positive integers
+    :param a: int type; positive integer
+    :param b: int type; positive integer
+    :returns: dict type; a dictionary with parameters a and b as keys,
+              and their corresponding Bezout coefficients as values.
+    :raises: ValueError if a < 0 or b < 0
+    """
+    if a < 0 or b < 0:
+        raise ValueError(
+        f"bezout_coeffs(a, b) does not support negative arguments.")
+    s0 = 0
+    t0 = 0
+    s1 = -1 * (b // a)
+    t1 = 1
 
-  temp = b
-  bk = a
-  ak = temp % a
+    temp = b
+    bk = a
+    ak = temp % a
 
-  while ak != 0:
-    temp_s = s1
-    temp_t = t1
+    while ak != 0:
+        temp_s = s1
+        temp_t = t1
 
-    # FIXME: Update s1 according to the formula for sk
-    s1 = "FIXME: Replace this string"
+    # Update s1 according to the formula for sk
+    s1 = s0 - temp_s * (bk // ak)
 
-    # FIXME: Update t1 according to the formula for tk
-    t1 = "FIXME: Replace this string"
+    # Update t1 according to the formula for tk
+    t1 = t0 - temp_t * (bk // ak)
 
     s0 = temp_s
     t0 = temp_t
     temp = bk
 
-    # FIXME: Update bk and ak
+    # Update bk and ak
     bk = "FIXME: Replace this string"
     ak = "FIXME: Replace this string"
 
   # FIXME: Replace each string with the correct coefficients of a and b
-  return {a: "FIXME: replace this string", b: "FIXME: replace this string"}
+    return {a: "FIXME: replace this string", b: "FIXME: replace this string"}
 
 
 """ ----------------- PROBLEM 3 ----------------- """
